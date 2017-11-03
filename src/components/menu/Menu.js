3acc5a8298438c66910a853cb16a9fad2ac1cd2f
inafalcao <inafalcao@gmail.com>
2017-11-03 10:46:53 -0300

    Changing Link to NavLink.

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './Menu.css'
 import iconRead from './ic_read_white.svg'
 import iconDash from './ic_dashboard_white.svg'
@@ -12,22 +12,19 @@ class Menu extends Component {
 			<nav className="menu">
 			  <ul>
 					<li>
-						<Link to="/weather">
+						<NavLink to="/weather">
 							<img src={iconLayers} className="icon"/>
 								Clima
-						</Link>
-						<span className="badge red">13</span>
-						{/* <ul className="sub">
-							<li>teste</li>
-						</ul> */}
+							<span className="badge red">13</span>	
+						</NavLink>
 					</li>
 					<li>
-						<Link to="/posts"><img src={iconRead} className="icon"/>
-						Posts
-						</Link>
-						<span className="badge green">50</span>
+						<NavLink to="/posts"><img src={iconRead} className="icon"/>
+							Posts
+							<span className="badge green">50</span>
+						</NavLink>
 					</li>
-					<li><Link to="/post"><img src={iconDash} className="icon"/>Postar</Link></li>
+					<li><NavLink to="/post"><img src={iconDash} className="icon"/>Postar</NavLink></li>
 			  </ul>
 			</nav>
 		)
